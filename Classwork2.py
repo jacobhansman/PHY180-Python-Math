e2f595fb1d051f268bc013cdd4e4dbe9d4a61268
jacobhansman <80072020+jacobhansman@users.noreply.github.com>
2021-03-04 16:56:57 -0500

    Add files via upload

---
+++ Classwork2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a=float(input('Please input the value for a: \n'))
+b=float(input('Please input the value for b: \n'))
+y=((3*(a**b))-((2*a)/b)+(6*(a-b)))
+print('')
+print('The value of the algebraic expression with the values inputted is y =', y)
